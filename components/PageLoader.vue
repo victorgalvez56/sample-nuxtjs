f447684695c5875ca4be19ace135428b4f729d79
Victor Galvez <victor.galvez56@gmail.com>
2021-06-02 00:58:09 -0500

    Test loader gif code

--- components/PageLoader.vue
+++ components/PageLoader.vue
@@ -1,7 +1,7 @@
 
  <template>
   <div class="page-loader" v-if="!isloaded">
-    <img src="kakashi.png"  class="cube" alt="">
+    <img src="code.gif"  class="cube" alt="">
   </div> 
 </template>
 
@@ -13,12 +13,13 @@
       }
     },
     mounted() {
-      document.onreadystatechange = () => {
-        if (document.readyState == "complete") { 
-          this.isloaded = true;
-        } 
-      }
+        this.activate()
     },
+    methods: {
+  activate() {
+    setTimeout(() => this.isloaded = true, 800);
+  }
+}
   }
 </script>
 
@@ -32,49 +33,15 @@
     left: 0;
     width: 100vw;
     height: 100vh;
-    background-color: black;
+    background-color: white;
     z-index: 999;
   }
   // -----------------------------------------------------
   .cube{
-    width: 150px;
-    height:150px;
+    width: 450px;
+    height:450px;
     margin-right: 10px;
   }
   // -----------------------------------------------------
- @-webkit-keyframes cube /* Safari and Chrome */ {
-  from {
-    -webkit-transform: rotate(0deg);
-    -o-transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-  to {
-    -webkit-transform: rotate(360deg);
-    -o-transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
-}
-@keyframes cube {
-  from {
-    -ms-transform: rotate(0deg);
-    -moz-transform: rotate(0deg);
-    -webkit-transform: rotate(0deg);
-    -o-transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-  to {
-    -ms-transform: rotate(360deg);
-    -moz-transform: rotate(360deg);
-    -webkit-transform: rotate(360deg);
-    -o-transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
-}
-.cube {
-  -webkit-animation: cube 2s linear infinite;
-  -moz-animation: cube 2s linear infinite;
-  -ms-animation: cube 2s linear infinite;
-  -o-animation: cube 2s linear infinite;
-  animation: cube 2s linear infinite;
-}
+
 </style>
